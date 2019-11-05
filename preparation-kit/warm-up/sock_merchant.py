178351199b7ed7939b65dda297350dfb1f1df93e
dancastillo <dancastillo99@gmail.com>
2019-11-04 21:50:36 -0500

    added sock merchant solution

---
+++ preparation-kit/warm-up/sock_merchant.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    count = 0
+    countArr = []
+
+    for i in range(len(ar)):
+        if ar[i] in countArr:
+            count += 1
+            countArr.remove(ar[i])
+        else:
+            countArr.append(ar[i])
+
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
